81504a7ad8b44c36f9a6050171a6699966665b30
Anthony Maton <maitre.negi@gmail.com>
2017-02-07 15:07:16 +0100

    Refactor db initialization script

--- backend/Softwarify/server/migrateDB.js
+++ backend/Softwarify/server/migrateDB.js
@@ -1,6 +1,13 @@
 var server = require('./server');
 var ds = server.dataSources.database;
-//var dataSource = app.dataSources.database;
+
+var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+ds.automigrate(lbTables, function(er) {
+    if (er) throw er;
+    console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+    ds.disconnect();
+});
+
 ds.autoupdate(null, function (err) {
   if(err) return err;
   return true;
