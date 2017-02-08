8df44cf307bdc51f7b6ba8dd9137c2063dd314e6
Anthony Maton <maitre.negi@gmail.com>
2017-02-08 11:43:01 +0100

    Edit profile IHM

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,10 @@ import App from './App';
 import Login from './Login';
 import Identifier from './Identifier';
 import AddProfile from './AddProfile';
+import EditProfile from './EditProfile';
 import './index.css';
 
 ReactDOM.render(
-  <AddProfile />,
+  <EditProfile />,
   document.getElementById('root')
 );
