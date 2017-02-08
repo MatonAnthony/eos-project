4b44a02d0bd234a4dc343d70d2bae00550cdbaf0
Gabriel Delhaye <gab.git@hotmail.com>
2017-02-08 14:20:18 +0100

    Generate client's password

--- backend/Softwarify/common/models/client.js
+++ backend/Softwarify/common/models/client.js
@@ -1,5 +1,23 @@
 'use strict';
+const generator = require('generate-password');
 
 module.exports = function(Client) {
-
+  //Will generate a random password when a client is created
+  Client.observe('before save', function generatePassword(ctx, next) {
+    if (ctx.instance){
+      //Ensure that it's a creation operation and not an update. It checks if a client with this identifier is already created
+     let exists = Client.exists(ctx.instance.identifier,
+       function(err, exists){
+          if(!exists){
+            let password = generator.generate({
+              length: 10,
+              number: true
+            });
+            ctx.instance.password = password;
+            next();
+          }
+        }
+      );
+    }
+  });
 };
