a703b403a7a8bcece3ce437d77447813f210d3f8
nourz02 <hoffmann.xav@gmail.com>
2017-02-09 15:14:21 +0100

    add /api before each api call & add a bash script for add all profiles

---
+++ backend/Softwarify/tmp/import_section.bash
@@ -0,0 +1,69 @@
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "acronyme": "1BIN", 
+   "full_name": "1er informatique" 
+ }' 'http://localhost:3000/api/Profiles'
+
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "acronyme": "2BIN", 
+   "full_name": "2eme informatique" 
+ }' 'http://localhost:3000/api/Profiles'
+
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "acronyme": "3BIN", 
+   "full_name": "3eme informatique" 
+ }' 'http://localhost:3000/api/Profiles'
+
+
+
+
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "acronyme": "1DIET", 
+   "full_name": "1er dietetique" 
+ }' 'http://localhost:3000/api/Profiles'
+
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "acronyme": "2DIET", 
+   "full_name": "2eme dietetique" 
+ }' 'http://localhost:3000/api/Profiles'
+
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{  
+   "acronyme": "3DIET", 
+   "full_name": "3eme dietetique" 
+ }' 'http://localhost:3000/api/Profiles'
+
+
+
+
+
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ 
+   "acronyme": "1BDI", 
+   "full_name": "1er inconnu" 
+ }' 'http://localhost:3000/api/Profiles'
+
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ 
+   "acronyme": "2BDI", 
+   "full_name": "2eme inconnu" 
+ }' 'http://localhost:3000/api/Profiles'
+
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ 
+   "acronyme": "3BDI", 
+   "full_name": "3eme inconnu" 
+ }' 'http://localhost:3000/api/Profiles'
+
+
+
+
+ curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ 
+   "acronyme": "1BBM", 
+   "full_name": "1er imagerie" 
+ }' 'http://localhost:3000/api/Profiles'
+
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ 
+   "acronyme": "2BBM", 
+   "full_name": "2eme imagerie" 
+ }' 'http://localhost:3000/api/Profiles'
+
+  curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{ 
+   "acronyme": "3BBM", 
+   "full_name": "3eme imagerie" 
+ }' 'http://localhost:3000/api/Profiles'
