038ac7452cbc43c91f76391fcbefbb7b861505aa
gabygit <gabygit@users.noreply.github.com>
2017-02-09 12:45:33 +0100

    Downloading of scripts + verification of export_format (#35)

--- backend/Softwarify/common/models/ressource.js
+++ backend/Softwarify/common/models/ressource.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Ressource) {
-
+  Ressource.observe('before save', function checkExportFormat(ctx, next) {
+    if (ctx.instance){
+      var format = ctx.instance.export_format;
+      if(format.charAt(0) !=  '.'){
+        format = '.' + format;
+        ctx.instance.export_format = format;
+      }
+    }
+    next();
+  });
 };
+
